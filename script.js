1493216fc24a7f988c28067d56616b8c736a0dbe
Chaotic-V01D <87347377+Chaotic-V01D@users.noreply.github.com>
2022-06-23 15:09:08 -0500

    added condition for a blank entity image

--- script.js
+++ script.js
@@ -18,7 +18,11 @@ let player={
   yPos:0,
   width:20,
   height:20,
-  renderDist: 100
+  renderDist: 100,
+  src:"",
+  render: function(){
+
+  }
 }
 function Entity(xPos, yPos, xVel, yVel, defCol, width, height, src){
   this.xPos = xPos
@@ -31,6 +35,7 @@ function Entity(xPos, yPos, xVel, yVel, defCol, width, height, src){
   this.src = src
   this.render = function(){
     if (checkEntityDist(player.xPos, player.yPos, this.xPos, this.yPos, this.width, this.height)<player.renderDist){
+      if (!(this.src=="")){
     let img = new Image();
     img.src = this.src;
     if (img.complete) {
@@ -42,7 +47,16 @@ function Entity(xPos, yPos, xVel, yVel, defCol, width, height, src){
         this.height
       );
     }
+  }else{
+    ctx.fillStyle=this.defCol
+    ctx.fillRect(
+      0 - player.x + (canvas.width / 2 - player.width / 2),
+      0 - player.y + (canvas.height / 2 - player.height / 2),
+      this.width,
+      this.height
+    );
   }
+}
 }
 }
 
