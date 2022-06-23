68c7b440156f9a52c111e968e83b9e2c1d67b49f
Chaotic-V01D <87347377+Chaotic-V01D@users.noreply.github.com>
2022-06-23 14:43:29 -0500

    entity render script

--- script.js
+++ script.js
@@ -13,17 +13,37 @@ document.onkeyup = function (e) {
   e = e || window.event;
   buttons[e.keyCode] = false;
 };
-
-function Entity(xPos, yPos, xVel, yVel, defCol, src){
+let player={
+  xPos: 0,
+  yPos:0,
+  width:20,
+  height:20,
+  renderDist: 100
+}
+function Entity(xPos, yPos, xVel, yVel, defCol, width, height, src){
   this.xPos = xPos
   this.yPos = yPos
   this.xVel = xVel
   this.yVel = yVel
   this.defCol = defCol
+  this.width = width
+  this.height = height
   this.src = src
   this.render = function(){
-
+    if (checkEntityDist(player.xPos, player.yPos, this.xPos, this.yPos, this.width, this.height)<player.renderDist){
+    let img = new Image();
+    img.src = this.src;
+    if (img.complete) {
+      ctx.drawImage(
+        img,
+        0 - player.x + (canvas.width / 2 - player.width / 2),
+        0 - player.y + (canvas.height / 2 - player.height / 2),
+        this.width,
+        this.height
+      );
+    }
   }
+}
 }
 
 function checkEntityDist(startX, startY, entX, entY, entWidth, entHeight){
